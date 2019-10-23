450a932d2d7b0ec941750e2439fca329b4e6fcc5
lucylp <56699398+lucylp@users.noreply.github.com>
2019-10-23 11:50:00 -0700

    Changed Background
    Earlier, I added a speed for the Balls, and added a new class called Turtle

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -13,7 +13,7 @@ function setup() {
 
 
 function draw(){
-	background(110, 100, 255);
+	background(110, 110, 255);
     b.drawBall(); //draw the ball called b (go look in the Ball class for the drawBall function)
     b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
     anotherBall.drawBall();
