a4c2fb987b8db4e567aac0156273d6c996eeb286
lucylp <56699398+lucylp@users.noreply.github.com>
2019-10-29 15:27:18 -0700

    Update sketch.js
    I built a game where you move horizontally and try to hit the balls raining from the top.

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -9,7 +9,7 @@ function setup() {
   createCanvas(500, 400);
 
   //make one avatar called me
-  me = new Avatar(width/2, 300, 3);
+  me = new Avatar(width/2, 300, 4);
 
 }
 
@@ -19,8 +19,8 @@ function draw(){
   me.drawMe();
   me.moveMe();
 
-  if (frameCount % 25 == 0) {
-      let  b = new Ball(width, random(0,height), -3);
+  if (frameCount % 35 == 0) {
+      let  b = new Ball(random(0,width), 0, -3);
       balls.push(b);
       console.log(balls); //print the balls array to the console
     }
@@ -44,25 +44,25 @@ class Avatar {
 	}
 
 	drawMe(){  // draw the running person
-    		stroke("green");
-        strokeWeight(3);
-    		fill("blue");
+    		stroke("yellow");
+        strokeWeight(5);
+    		fill("yellow");
 		    ellipse(this.x,this.y,20,20);
         line(this.x,this.y, this.x, this.y+40);
         line(this.x, this.y+40, this.x-20, this.y+60);
         line(this.x, this.y+40, this.x+10, this.y+50);
         line(this.x+10, this.y+50, this.x+5, this.y+60);
         line(this.x, this.y+15, this.x-10, this.y+25);
-        line(this.x-10, this.y+25, this.x+10, this.y+35);
+        line(this.x-10, this.y+25, this.x, this.y+30);
 	}
 
 	moveMe(){
-    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
-       this.y -= this.speed;
+    if (keyIsDown(LEFT_ARROW)) { //if you hold the up arrow, move left by speed
+       this.x = this.x - this.speed;
     }
 
-    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
-        this.y += this.speed;
+    if (keyIsDown(RIGHT_ARROW)) { // if you hold the down arrow, move right by speed
+        this.x += this.speed;
     }
 	}
 
@@ -86,15 +86,15 @@ class Ball {
 	// draw a ball on the screen at x,y
 	drawBall(){
     	stroke(0);
-      strokeWeight(1);
-    	fill("red");
+      strokeWeight(0);
+    	fill("blue");
 		  ellipse(this.x,this.y,10,10);
 	}
 
 	//update the location of the ball, so it moves across the screen
 	moveBall(){
-		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
+		this.x = this.x;
+		this.y = this.y-this.speed;
 	}
 
 	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
